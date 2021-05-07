7eb514a8ce0f22d2b9061ba43a6dc77d4f3f087b
Pablo Speroni <pablolsperoni@gmail.com>
2021-05-07 18:06:04 -0300

    proyecto base

---
+++ TiendaUno/TiendaUno.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace TiendaUno.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new TiendaUno.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ TiendaUno/TiendaUno.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace TiendaUno.Skia.Gtk
+{
+}
